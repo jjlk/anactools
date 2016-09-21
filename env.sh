32a5dc7ced1f274c526572e1d8f7450e1aebe277
hijlk <julien.lefaucheur@obspm.fr>
2016-09-21 15:14:32 +0200

    change env.sh

--- env.sh
+++ env.sh
@@ -5,8 +5,7 @@ export GAMMALIB=$TOOLS/python/gamma
 export CTOOLS=$TOOLS/python/gamma
 
 ### Path to data
-#export VHEFITS=/Users/julien/Documents/WorkingDir/Tools/python/anactools/data/hess/prod/fits/prod_pa/pa_2015-07/Model_Deconvoluted_Prod26/Mpp_Std
-export VHEFITS=/Users/julien/Documents/WorkingDir/Tools/python/anactools/data/hess/
+export VHEFITS=$ANACTOOLS/data/hess/
 
 ### envs to work with gammalib/ctools
 source $GAMMALIB/bin/gammalib-init.sh
